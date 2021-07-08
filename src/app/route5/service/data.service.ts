6ec915268a6309691c537ad9d8955f2ee9bc9ee9
Aditya Chauhan <adichauh4@publicisgroupe.net>
2021-07-08 10:06:38 +0530

    service layer integration

--- src/app/route5/service/data.service.ts
+++ src/app/route5/service/data.service.ts
@@ -5,9 +5,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DataService {
 
+    readonly DATA_URL = "https://my-json-server.typicode.com/aDeceiver/json-data/students"
+
     constructor(private http: HttpClient) { }
 
     getRecords() {
-        return this.http.get('assets/stub/record.json');
+        return this.http.get(this.DATA_URL);
     }
 }
